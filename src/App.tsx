6329f1449d38a121cc825d0a116344f8bac5099a
lipika1080 <sinhalipika2000@gmail.com>
2025-05-23 11:59:53 +0800

    p01 frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, VStack, Heading, Textarea, useToast, HStack } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import SavedSummaries from './SavedSummaries';
 
--- src/SavedSummaries.tsx
+++ src/SavedSummaries.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Heading, VStack, Text, Spinner, Divider } from '@chakra-ui/react';
 import axios from 'axios';
 
